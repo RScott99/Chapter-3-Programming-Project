ef3ff602b887932dfe651c1f598479f75e6ef9af
RScott99 <rsh06.rh@gmail.com>
2025-06-29 13:12:50 -0400

    started work on interest program

--- Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project.cpp
@@ -6,20 +6,10 @@ using namespace std;
 int main()
 {
 	//defining variables
-	int numCookies;
-	double sugarCups, butterCups, flourCups, cookieRatio;
-	//querying number of cookies
-	std::cout << "How many cookeis do you want to make?  ";
-	std::cin >> numCookies; //assigning number to numCookies
-	//calculating ingrediants needed
-	cookieRatio = (numCookies / 48.0);
-	sugarCups = 1.5 * cookieRatio;
-	butterCups = 1 * cookieRatio;
-	flourCups = 2.75 * cookieRatio;
-	//outputing results
-	std::cout << "\nTo make " << numCookies << " cookies, you will need:\n";
-	std::cout << "Cups of Sugar: " << sugarCups << endl;
-	std::cout << "Cups of Butter: " << butterCups << endl;
-	std::cout << "Cups of Flour: " << flourCups << endl;
+	int timesCompound;
+	double interestRate, principal, interestAmmount, savingsAmmount;
+
+	std::cout << 
+	
 	return 0;
 }
