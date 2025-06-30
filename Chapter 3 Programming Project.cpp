09f502de5454644301dc4b5ec812ec04feb0a79a
RScott99 <rsh06.rh@gmail.com>
2025-06-29 20:56:19 -0400

    fixed code, and added comments and the code works as intended

--- Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project.cpp
@@ -1,15 +1,34 @@
-// Chapter 3 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <cmath>
+#include <iomanip>
 #include <iostream>
 using namespace std;
+
 int main()
 {
-	//defining variables
 	int timesCompound;
-	double interestRate, principal, interestAmmount, savingsAmmount;
-	std::cout << "Enter your principal: ";
-	std::cin >> principal;
-	
+	double interestRate, principal, interestAmount, savingsAmount;
+
+	// Input
+	cout << "Enter your principal: ";
+	cin >> principal;
+	cout << "\nEnter your interest rate: ";
+	cin >> interestRate;
+	interestRate = interestRate / 100;  // Convert from percent to decimal
+	cout << "\nEnter the number of times the interest is compounded per year: ";
+	cin >> timesCompound;
+
+	// Calculate compound interest
+	double temp = pow((1 + (interestRate / timesCompound)), timesCompound);
+	savingsAmount = principal * temp;
+	interestAmount = savingsAmount - principal;
+
+	// Output
+	cout << fixed << setprecision(2);
+	cout << left << setw(20) << "\nInterest Rate:" << right << setw(20) << (interestRate * 100) << "%" << endl;
+	cout << left << setw(20) << "Times Compounded:" << right << setw(20) << timesCompound << endl;
+	cout << left << setw(20) << "Principal:" << right << setw(12) << "$" << setw(8) << fixed << setprecision(2) << principal << endl;
+	cout << left << setw(20) << "Interest:" << right << setw(12) << "$" << setw(8) << fixed << setprecision(2) << interestAmount << endl;
+	cout << left << setw(20) << "Amount in Savings:" << right << setw(12) << "$" << setw(8) << fixed << setprecision(2) << savingsAmount << endl;
+
 	return 0;
 }
